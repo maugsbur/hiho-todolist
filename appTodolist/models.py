5eba816f7126c1ba29d39f8fd90a8352aad08443
Nico Delgado <nsdelgadov@gmail.com>
2017-03-20 16:31:10 +0000

    WIP: add_task (error ñ)

--- appTodolist/models.py
+++ appTodolist/models.py
@@ -2,9 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Create your models here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+'''
 class Task(models.Model):
     
     name = models.CharField(max_length=20)
     
+class TasksList(models.Model):
+    # https://docs.djangoproject.com/en/1.10/topics/db/models/
+    # models.oneToManyField(Task, )
+    
+    
+    def is_empty(self):
+        return len(tasks.all()) < 1
+    
+    def add_task(self):
+
+''' 
--- appTodolist/tests.py
+++ appTodolist/tests.py
@@ -1,23 +1,63 @@
 from django.test import TestCase
 from appTodolist.models import *
 
-# Create your tests here.
+# Create your tests here!
 
+class Task:
+    def __init__(self, name):
+        self.name = name
+        self.done = False
+        
+class TasksList:
+    def __init__(self):
+        self.tasks = []
+        
+    def is_empty(self):
+        return len(self.tasks) == 0
+        
+    def add_task(self, new_task):
+        self.tasks.append(new_task)
+        
+    def size(self):
+        return len(self.tasks)
+        
 class TodoTest (TestCase):
     
     def test_empty(self):
         #arrange
+        t = TasksList()
         #act
         #assert
-        self.assertEquals(0, Task.objects.all().count())
+        self.assertTrue(t.is_empty())
         
     def test_add_task(self):
+        '''
+        add task, is empty, 
+        '''
         #arrange
-        t = Task.objects.create(name="Task 1")
+        tasks_list = TasksList()
+        task = Task(name="name!!")
         #act
+        tasks_list.add_task(task)
         #assert
-        self.assertEquals(1, Task.objects.all().count())
-
+        self.assertFalse(tasks_list.is_empty())
+        self.assertEquals(1, tasks_list.size())
+        #self.assertEquals(task, tasks_list.get_last())
+    
+    def test_weird_encoding(self):
+        flag = False
+        #arrange
+        tasks_list = TasksList()
+        try:
+            task = Task(name="ñ")
+        except:
+            flag = True
+        #act
+        tasks_list.add_task(task)
+        #assert
+        assertTrue(flag)
+    
+    '''
     def test_list_tasks(self):
         #arrange
         t1 = Task.objects.create(name="Task 1")
@@ -28,4 +68,4 @@ class TodoTest (TestCase):
         #assert
         self.assertEquals([t1, t2, t3], list(Task.objects.all()))
         
-     
+     '''
