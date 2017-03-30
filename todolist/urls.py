8be16d21583508b00a1ab0b5402493a9415077d8
Nico Delgado <nsdelgadov@gmail.com>
2017-03-30 22:37:12 +0000

    Add hello world view

--- todolist/urls.py
+++ todolist/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
-from appTodoList import views
+from appTodolist import views
 from django.contrib import admin
 
 urlpatterns = [
